b0841ec33692c0accd7774f578d8b4d983509d16
Ayush Gupta <ayushgupta555544@gmail.com>
2019-06-27 18:35:41 +0530

    Added Screen3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { Screen1Component } from './components/screen1/screen1.component';
 import { Screen2Component } from './components/screen2/screen2.component';
+import { Screen3Component } from './components/screen3/screen3.component';
 
 const routes: Routes = [{
   path : 'screen1',
   component : Screen1Component
 },
 {
-  path : '**',
+  path : 'screen2',
   component : Screen2Component
+},
+{
+  path : '**',
+  component : Screen3Component
 }];
 
 @NgModule({
--- src/app/components/screen3/screen3.component.ts
+++ src/app/components/screen3/screen3.component.ts
@@ -12,4 +12,80 @@ export class Screen3Component implements OnInit {
   ngOnInit() {
   }
 
+  out: object = {}
+  calculate(text) {
+
+    Promise.all([this.countWords(text), this.countArticles(text), this.countVowels(text),
+       this.getMaxLength(text), this.getMinLength(text)])
+      .then(d => {
+          this.out['wordCount'] = d[0],
+          this.out['articleCount'] = d[1],
+          this.out['vowelCount'] = d[2],
+          this.out['maxWordLength'] = d[3],
+          this.out['minWordLength'] = d[4]
+      })
+      .catch(e => {
+        console.log(e)
+      })
+  }
+
+  async countWords(text: string): Promise<number> {
+    let count = 0
+    if (text !== "") {
+      count = await text.split(/\/| |,/).length
+    }
+    return count
+  }
+
+  async countArticles(text): Promise<number> {
+    let temp1: string[] = await text.split(/\/| |,/).filter(e => {
+      const articles = ['a', 'an', 'the']
+      e.toLowerCase()
+      return articles.includes(e)
+    })
+    return temp1.length
+  }
+
+  async getWordsWithoutArticles(text): Promise<string[]> {
+    return await text.split(/\/| |,/).filter(e => {
+      const articles = ['a', 'an', 'the']
+      e.toLowerCase()
+      return !articles.includes(e)
+    })
+  }
+
+  async countVowels(text): Promise<number> {
+    let vowelCount: number = 0
+    const wordsWithoutArticles = await this.getWordsWithoutArticles(text)
+    wordsWithoutArticles.forEach(e => {
+      vowelCount += e.split("").filter(char => {
+        const vowels = ['a', 'e', 'i', 'o', 'u']
+        char.toLowerCase()
+        return vowels.includes(char)
+      }).length
+    })
+    return vowelCount
+  }
+
+  async getMaxLength(text): Promise<number> {
+    let maxLength: number = 0
+    const wordsWithoutArticles = await this.getWordsWithoutArticles(text)
+    wordsWithoutArticles.forEach(word => {
+      if (word.length > maxLength) {
+        maxLength = word.length
+      }
+    })
+    return maxLength
+  }
+
+  async getMinLength(text): Promise<number> {
+    const wordsWithoutArticles = await this.getWordsWithoutArticles(text)
+    let minLength: number = wordsWithoutArticles[0].length
+    wordsWithoutArticles.forEach(word => {
+      if (word.length < minLength) {
+        minLength = word.length
+      }
+    })
+    return minLength
+  }
 }
